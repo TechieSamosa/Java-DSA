a81b3d6e11772b8242a124a8bb4079607de6cdba
TheNaiveSamosa <khamitakaraditya@gmail.com>
2024-08-10 18:34:53 +0530

    Day 1

--- Java Basics/Patterns, Functions & Methods/DeciToBin.java
+++ Java Basics/Patterns, Functions & Methods/DeciToBin.java
@@ -1,18 +1,20 @@
 public class DeciToBin {
-    public static void DecToBin(int n){
+    public static void BinToDec(int n){
         int num = n;
         int pow = 0;
         int deciNum = 0;
-        while(n>0){int lastDigit = n % 10;
-            deciNum = deciNum + (lastDigit*(int)Math.pow(2,pow));
-            n = n/10;
-            pow = pow + 1;
+
+        while(n > 0){
+            int lastDigit = n % 10;
+            deciNum = deciNum + (lastDigit * (int)Math.pow(2, pow));
+            n = n / 10;
+            pow++;
+        }
+
+        System.out.println("The Decimal of the given Binary " + num + " is " + deciNum);
     }
-            
-        System.out.println("The Decimal of the given Binary" + num + "is"+ deciNum);
-    }
+
     public static void main(String args[]){
-        DecToBin(101);
+        BinToDec(101);
     }
-    
 }
