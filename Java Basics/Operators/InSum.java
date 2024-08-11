a9999810bf780c2af15b5fdc1e5131dbc7603eb6
TheNaiveSamosa <khamitkaraditya@gmail.com>
2024-08-11 10:58:24 +0530

    Input

---
+++ Java Basics/Operators/InSum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class InSum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st Number");
+        int a = sc.nextInt();
+        System.out.println("Enter the 2nd Number");
+        int b = sc.nextInt();
+        int c = (a+b);
+        System.out.println("The Sum of both numbers is :"+c);
+    }
+}
